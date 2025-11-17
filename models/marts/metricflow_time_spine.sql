14bd1b9a4efd1fe30e9a60d5a1e2ec89b1511f04
Miles Freeborn <mfreeborn@dbtlabs.com>
2025-11-17 16:03:10 -0500

    Fix time spine: use dbt_date.get_base_dates() and proper column config
    - Changed from dbt_utils.date_spine to dbt_date.get_base_dates()
    - Updated YAML to use columns with granularity instead of time_spine config
    - Matches working semantic layer syntax

--- models/marts/metricflow_time_spine.sql
+++ models/marts/metricflow_time_spine.sql
@@ -1,23 +1,20 @@
-{{
-    config(
-        materialized = 'table',
-    )
-}}
+-- metricflow_time_spine.sql
+with
 
-with days as (
-    {{
-        dbt_utils.date_spine(
-            'day',
-            "to_date('01/01/2000','mm/dd/yyyy')",
-            "to_date('01/01/2027','mm/dd/yyyy')"
-        )
-    }}
+days as (
+
+    --for BQ adapters use "DATE('01/01/2000','mm/dd/yyyy')"
+    {{ dbt_date.get_base_dates(n_dateparts=365*10, datepart="day") }}
+
 ),
 
-final as (
+cast_to_date as (
+
     select cast(date_day as date) as date_day
+
     from days
+
 )
 
-select * from final
+select * from cast_to_date
 
